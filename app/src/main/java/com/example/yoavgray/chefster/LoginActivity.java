322fedcaea247573e48515043c2ae8c4a79a3123
Yoav Gray <yoav@autolist.com>
2016-11-08 20:58:45 -0800

    Added Crashlytics

--- app/src/main/java/com/example/yoavgray/chefster/LoginActivity.java
+++ app/src/main/java/com/example/yoavgray/chefster/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.yoavgray.chefster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_login);
     }
 }
