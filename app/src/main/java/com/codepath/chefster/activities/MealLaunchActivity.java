fdd2588cbd685c44424a305af798192e51432cda
Yoav Gray <yoav@autolist.com>
2016-11-13 18:06:57 -0800

    Added a stub for the activity

---
+++ app/src/main/java/com/codepath/chefster/activities/MealLaunchActivity.java
@@ -0,0 +1,13 @@
+package com.codepath.chefster.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealLaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_launch);
+    }
+}
